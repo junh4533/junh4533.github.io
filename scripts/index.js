625e49086bb926de63921a0b3f3373a38aa41b27
Jun <junh4533@gmail.com>
2020-02-02 01:59:21 -0500

    made sag map full screen

--- scripts/index.js
+++ scripts/index.js
@@ -52,46 +52,20 @@ $(document).ready(function () {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 
-    // var starfield = document.getElementById("starfield"),
-    //     ctx = starfield.getContext("2d"),
-    //     mouseTravel = 15,
-    //     mouseX = 0,
-    //     mouseY = 0;
+    var starfield = document.getElementById("starfield"),
+        mouseTravel = 15,
+        mouseX = 0,
+        mouseY = 0;
 
-    // //set canvas to fullscreen
-    // starfield.width = document.body.clientWidth;
-    // starfield.height = document.body.clientHeight;
-
-    // document.body.addEventListener("mousemove", function (e) {
-    //     //if mouse moves the minimum x or y distance
-    //     if (Math.abs(e.pageX - mouseX) > mouseTravel || Math.abs(e.pageY - mouseY) > mouseTravel) {
-    //         mouseX = e.pageX;
-    //         mouseY = e.pageY;
-
-    //         var x = getRandom(-50, 50) + e.pageX,
-    //             y = getRandom(-50, 50) + e.pageY,
-    //             radius = Math.random() * 2,
-    //             // radius = Math.random() > .99 ? Math.random() * 20 : Math.random() * 2,
-    //             saturation = getRandom(50, 100), //higher range = higher chance of darker stars
-    //             lightness = getRandom(70, 100); //higher range = lower chance of being 100% white
-
-    //         ctx.beginPath();
-    //         ctx.arc(x, y, radius, 0, 360); //draws a circle
-    //         ctx.shadowBlur = lightness / 10;
-    //         ctx.shadowColor = "white";
-    //         ctx.fillStyle = "hsl(240, " + saturation + "%, " + lightness + "%)";
-    //         ctx.fill();
-    //     }
-
-    // })
-
+    //set canvas to fullscreen
+    starfield.width = document.body.clientWidth;
+    starfield.height = document.body.clientHeight;
 
     var sagittarius = document.getElementById("sagittarius"),
-        ctx = sagittarius.getContext("2d"),
         canvasWH = document.body.clientHeight,
         mouseDistance = 60;
         
-    sagittarius.width = canvasWH;
+    sagittarius.width = document.body.clientWidth;
     sagittarius.height = canvasWH;
 
     // x times a square canvasWH will give coordinate
@@ -119,52 +93,47 @@ $(document).ready(function () {
 
     document.body.addEventListener("mousemove", function (e) {
 
-        for (var star in star_coordinates){
-            var starX = star_coordinates[star][0] * canvasWH;
-            var starY = star_coordinates[star][1] * canvasWH;
-            //if mouse moves within mouseDistance of the star
-            if (Math.abs(e.pageX - starX) <= mouseDistance && Math.abs(e.pageY - starY) <= mouseDistance) {
-                console.log(starX);
-                var radius = Math.random() * 5,
+        //if mouse moves the minimum x or y distance
+        if (Math.abs(e.pageX - mouseX) > mouseTravel || Math.abs(e.pageY - mouseY) > mouseTravel) {
+            mouseX = e.pageX;
+            mouseY = e.pageY;
+
+            var x = getRandom(-50, 50) + e.pageX,
+                y = getRandom(-50, 50) + e.pageY,
+                radius = Math.random() * 2,
+                // radius = Math.random() > .99 ? Math.random() * 20 : Math.random() * 2,
                 saturation = getRandom(50, 100), //higher range = higher chance of darker stars
                 lightness = getRandom(70, 100); //higher range = lower chance of being 100% white
 
+            ctx = starfield.getContext("2d");
+            ctx.beginPath();
+            ctx.arc(x, y, radius, 0, 360); //draws a circle
+            ctx.shadowBlur = lightness / 10;
+            ctx.shadowColor = "white";
+            ctx.fillStyle = "hsl(240, " + saturation + "%, " + lightness + "%)";
+            ctx.fill();
+        }
+
+        for (var star in star_coordinates){
+            var starX = star_coordinates[star][0] * document.body.clientWidth;
+            var starY = star_coordinates[star][1] * canvasWH;
+            //if mouse moves within mouseDistance of the star
+            if (Math.abs(e.pageX - starX) <= mouseDistance && Math.abs(e.pageY - starY) <= mouseDistance) {
+                var radius = 15,
+                saturation = getRandom(50, 100), //higher range = higher chance of darker stars
+                lightness = getRandom(70, 100); //higher range = lower chance of being 100% white
+
+                ctx = sagittarius.getContext("2d");
                 ctx.beginPath();
                 ctx.arc(starX, starY, radius, 0, 360); //draws a circle
                 ctx.shadowBlur = lightness / 10;
                 ctx.shadowColor = "white";
                 ctx.fillStyle = "hsl(240, " + saturation + "%, " + lightness + "%)";
                 ctx.fill();
+                delete star_coordinates[star];
             }
         }
 
     })
-
-
-    // ctx.beginPath();
-    // ctx.arc(x, y, 10, 0, 360); //draws a circle
-    // ctx.fillStyle = "white";
-    // ctx.fill();
-
-
-    // star1 = drawSagittarius(canvasWH * .4, canvasWH * .9);
-    // star2 = drawSagittarius(canvasWH * .39, canvasWH * .8);
-    // drawSagittarius(canvasWH * .234, canvasWH * .834);
-    // drawSagittarius(canvasWH * .212, canvasWH * .636);
-    // drawSagittarius(canvasWH * .2, canvasWH * .584);
-    // drawSagittarius(canvasWH * .194, canvasWH * .396);
-    // drawSagittarius(canvasWH * .336, canvasWH * .284);
-    // drawSagittarius(canvasWH * .476, canvasWH * .406);
-    // drawSagittarius(canvasWH * .456, canvasWH * .17);
-    // drawSagittarius(canvasWH * .402, canvasWH * .072);
-    // drawSagittarius(canvasWH * .508, canvasWH * .456);
-    // drawSagittarius(canvasWH * .534, canvasWH * .34);
-    // drawSagittarius(canvasWH * .588, canvasWH * .368);
-    // drawSagittarius(canvasWH * .684, canvasWH * .32);
-    // drawSagittarius(canvasWH * .744, canvasWH * .23);
-    // drawSagittarius(canvasWH * .73, canvasWH * .446);
-    // drawSagittarius(canvasWH * .81, canvasWH * .474);
-    // drawSagittarius(canvasWH * .694, canvasWH * .588);
-    // drawSagittarius(canvasWH * .74, canvasWH * .66);
 
 });
